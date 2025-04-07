9db23dc211781a0f090d39f8339bbdea2fa15d8f
Jeral Pineda <jeral.cris@gmail.com>
2025-04-06 21:00:47 -0600

    Initial Commit

---
+++ CursoIOS/CursoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoIOSApp.swift
+//  CursoIOS
+//
+//  Created by Jeral Pineda on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
