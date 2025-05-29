10bfd50f9e9bd9bc10dd8fbd499e8f283de945b6
Jeral Pineda <jeral.cris@gmail.com>
2025-05-28 21:43:47 -0600

    Add Charts on SuperheroDetail

--- CursoIOS/Superhero/SuperHeroDetail.swift
+++ CursoIOS/Superhero/SuperHeroDetail.swift
@@ -5,6 +5,7 @@
 //  Created by Jeral Pineda on 27/5/25.
 //
 
+import Charts
 import SDWebImageSwiftUI
 import SwiftUI
 
@@ -34,7 +35,7 @@ struct SuperHeroDetail: View {
                 ForEach(superhero.biography.aliases, id: \.self) { alias in
                     Text(alias).foregroundColor(.gray).italic()
                 }
-
+                SuperheroStats(stats: superhero.powerstats)
                 Spacer()
 
             }
@@ -60,6 +61,64 @@ struct SuperHeroDetail: View {
         }
     }
 
+}
+
+struct SuperheroStats: View {
+    let stats: ApiNetwork.Powerstats
+
+    var body: some View {
+        VStack {
+            Chart {
+                SectorMark(
+                    angle: .value("Count", Int(stats.combat) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                ).cornerRadius(5)
+                    .foregroundStyle(by: .value("Category", "Combat"))
+
+                SectorMark(
+                    angle: .value("Count", Int(stats.durability) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                ).cornerRadius(5)
+                    .foregroundStyle(by: .value("Category", "Durability"))
+
+                SectorMark(
+                    angle: .value("Count", Int(stats.intelligence) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                ).cornerRadius(5)
+                    .foregroundStyle(by: .value("Category", "Intelligence"))
+
+                SectorMark(
+                    angle: .value("Count", Int(stats.power) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                ).cornerRadius(5)
+                    .foregroundStyle(by: .value("Category", "Power"))
+
+                SectorMark(
+                    angle: .value("Count", Int(stats.speed) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                ).cornerRadius(5)
+                    .foregroundStyle(by: .value("Category", "Speed"))
+
+                SectorMark(
+                    angle: .value("Count", Int(stats.strength) ?? 0),
+                    innerRadius: .ratio(0.6),
+                    angularInset: 5
+                ).cornerRadius(5)
+                    .foregroundStyle(by: .value("Category", "Strength"))
+
+            }
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity, maxHeight: 350)
+        .background(.foregroundApp)
+        .cornerRadius(16)
+        .padding(24)
+    }
 }
 
 #Preview {
